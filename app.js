5513b9e1cad705a4c4a2feb4271e9d4209ca14cf
Akash.ziz <akash701043@gmail.com>
2024-08-26 11:02:22 +0530

    added some feastures

--- app.js
+++ app.js
@@ -11,3 +11,47 @@ setTimeout(() => {
 }, 200); // Change style back after 2 seconds
 
 })
+
+
+function playSound1() {
+  var clap = document.getElementById("clap");
+  clap.play();
+
+}
+
+function playSound2() {
+  var hihat = document.getElementById("hihat");
+  hihat.play();
+}
+
+function playSound3() {
+  var kick = document.getElementById("kick");
+  kick.play();
+}
+
+function playSound4() {
+  var openhat = document.getElementById("openhat");
+  openhat.play();
+}
+function playSound5() {
+  var boom = document.getElementById("boom");
+  boom.play();
+}
+function playSound6() {
+  var ride = document.getElementById("ride");
+  ride.play();
+}
+function playSound7() {
+  var snare = document.getElementById("snare");
+  snare.play();
+}
+
+function playSound8() {
+  var tom = document.getElementById("tom");
+  tom.play();
+}
+
+function playSound9() {
+  var pink = document.getElementById("pink");
+  pink.play();
+}
